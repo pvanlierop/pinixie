09ac7b5926312c8a578397d37b4be74edfc48c70
Paul Van Lierop <pevanlierop@gmail.com>
2016-01-31 07:18:42 +0000

    Update range to hit all of the shift registers

--- shiftester.py
+++ shiftester.py
@@ -1,9 +1,9 @@
 from shiftpi import HIGH, LOW, ALL, digitalWrite, delay
 
-digitalWrite(1, HIGH)
+digitalWrite(0, HIGH)
 delay(1000)
 
-digitalWrite(1, LOW)
+digitalWrite(0, LOW)
 
 delay(1000)
 
@@ -13,7 +13,7 @@ digitalWrite(ALL, LOW)
 
 
 while True:
-    for x in range(1, 8):
+    for x in range(0, 8):
         digitalWrite(x, HIGH)
-        delay(2000)
+        delay(100)
         digitalWrite(x, LOW)
