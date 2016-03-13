7a542a762c9cc613911f7474cc9cd2d988e57d48
pavanlie <pavanlie@cisco.com>
2016-03-13 11:10:35 -0700

    Updated intToBit using the format function to pad the appropriate amount of zero's to the nibble

--- clockmain.py
+++ clockmain.py
@@ -12,18 +12,29 @@ dataBit = 25
 LATCH = 24
 CLK = 23
 
+'''
+A lot going on in this one. Take an integer and make a 4 bit nibble out of it
+instead of using the bin() function we're using format to turn it into a 4 bit
+nibble with leading 0 bits if necessary, bin() doesn't do that, we then use 2: to
+strip off the 0x into our list
+'''
 def intToBit(n):
-    return [int(digit) for digit in bin(n)[2:]]
+    return [int(digit) for digit in format(n, '#06b')[2:]]
 
+#pulse the shift register clock
 def pulseCLK():
     GPIO.output(CLK, HIGH)
     GPIO.output(CLK, LOW)
     return
 
+#latch the shiftregister
 def serLatch():
     GPIO.output(LATCH, HIGH)
     GPIO.output(LATCH, LOW)
     return
+
+# Takes a 24 bit or less variable and outputs the appropriate nixie digits
+def nixiePush(bits):
 
 if __name__ == '__main__':
 
